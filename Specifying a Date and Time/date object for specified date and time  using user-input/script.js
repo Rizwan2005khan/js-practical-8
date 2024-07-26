a234707fdfa3ac9d79a397f72f54a0f8e770b9d1
Rizwan Ullah <rizwanullahicp0306@gmail.com>
2024-07-26 11:45:59 +0500

    Update script.js

--- Specifying a Date and Time/date object for specified date and time  using user-input/script.js
+++ Specifying a Date and Time/date object for specified date and time  using user-input/script.js
@@ -1,9 +1,9 @@
-let year = parseInt(prompt("Enter the year (e.g., 2022):"));
-let month = parseInt(prompt("Enter the month (1-12):")) - 1; 
-let day = parseInt(prompt("Enter the day (1-31):"));
-let hours = parseInt(prompt("Enter the hours (0-23):"));
-let minutes = parseInt(prompt("Enter the minutes (0-59):"));
-let seconds = parseInt(prompt("Enter the seconds (0-59):"));
+let year = parseInt(prompt("Enter the year"));
+let month = parseInt(prompt("Enter the month")) - 1; 
+let day = parseInt(prompt("Enter the day"));
+let hours = parseInt(prompt("Enter the hours"));
+let minutes = parseInt(prompt("Enter the minutes"));
+let seconds = parseInt(prompt("Enter the seconds"));
 
 let specificDate = new Date(year, month, day, hours, minutes, seconds);
 
